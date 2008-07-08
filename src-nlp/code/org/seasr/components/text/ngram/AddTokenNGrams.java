387127d4a0a23c53a350e1437898f9cb7fa834a3
Duane Searsmith <dsears@illinois.edu>
2008-07-08 17:42:48 +0000

    Comments.

--- src-nlp/code/org/seasr/components/text/ngram/AddTokenNGrams.java
+++ src-nlp/code/org/seasr/components/text/ngram/AddTokenNGrams.java
@@ -127,7 +127,7 @@ public class AddTokenNGrams implements ExecutableComponent {
 
 	// props
 
-	@ComponentProperty(description = "Verbose output?", name = "verbose", defaultValue = "false")
+	@ComponentProperty(description = "Verbose output? A boolean value (true or false).", name = "verbose", defaultValue = "false")
 	final static String DATA_PROPERTY_VERBOSE = "verbose";
 
 	@ComponentProperty(description = "Number of tokens per ngram?", name = "arity", defaultValue = "false")
