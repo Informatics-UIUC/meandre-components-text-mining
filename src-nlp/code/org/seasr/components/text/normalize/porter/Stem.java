49adbcfed331dc4a334e3fa85659fc47c7572269
Lily Dong <lilydong@illinois.edu>
2009-05-28 15:32:01 +0000

    convert to abstract component version and add output

--- src-nlp/code/org/seasr/components/text/normalize/porter/Stem.java
+++ src-nlp/code/org/seasr/components/text/normalize/porter/Stem.java
@@ -1,36 +1,36 @@
 /**
  * University of Illinois/NCSA
  * Open Source License
- * 
- * Copyright (c) 2008, Board of Trustees-University of Illinois.  
+ *
+ * Copyright (c) 2008, Board of Trustees-University of Illinois.
  * All rights reserved.
- * 
- * Developed by: 
- * 
+ *
+ * Developed by:
+ *
  * Automated Learning Group
  * National Center for Supercomputing Applications
  * http://www.seasr.org
- * 
- *  
+ *
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal with the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
  * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions: 
- * 
+ * furnished to do so, subject to the following conditions:
+ *
  *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimers. 
- * 
+ *    this list of conditions and the following disclaimers.
+ *
  *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimers in the 
- *    documentation and/or other materials provided with the distribution. 
- * 
+ *    this list of conditions and the following disclaimers in the
+ *    documentation and/or other materials provided with the distribution.
+ *
  *  * Neither the names of Automated Learning Group, The National Center for
  *    Supercomputing Applications, or University of Illinois, nor the names of
  *    its contributors may be used to endorse or promote products derived from
- *    this Software without specific prior written permission. 
- * 
+ *    this Software without specific prior written permission.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -63,12 +63,13 @@ import org.seasr.components.text.datatype.corpora.Annotation;
 import org.seasr.components.text.datatype.corpora.AnnotationConstants;
 import org.seasr.components.text.datatype.corpora.AnnotationSet;
 import org.seasr.components.text.datatype.corpora.Document;
+import org.meandre.components.abstracts.AbstractExecutableComponent;
 import org.meandre.core.*;
 import org.meandre.annotations.*;
 import org.seasr.components.text.util.feature_maps.FeatureValueEncoderDecoder;
 
 /**
- * 
+ *
  * <p>
  * Overview: <br>
  * This component transforms terms into their word stems. In this way, different
@@ -98,12 +99,12 @@ import org.seasr.components.text.util.feature_maps.FeatureValueEncoderDecoder;
  * Trigger Criteria: <br>
  * All.
  * </p>
- * 
- * 
+ *
+ *
  * @author D. Searsmith
- * 
+ *
  * TODO: Testing, Unit Tests
- * 
+ *
  */
 @Component(creator = "Duane Searsmith",
 
@@ -134,7 +135,7 @@ description = "<p>Overview: <br>"
 
 name = "Stem", tags = "nlp text document normalize stem",
 baseURL="meandre://seasr.org/components/")
-public class Stem implements ExecutableComponent {
+public class Stem  extends AbstractExecutableComponent {
 
 	// ==============
 	// Data Members
@@ -173,13 +174,15 @@ public class Stem implements ExecutableComponent {
 		return Boolean.parseBoolean(s.toLowerCase());
 	}
 
-	public void initialize(ComponentContextProperties ccp) {
+	public void initializeCallBack(ComponentContextProperties ccp)
+    throws Exception {
 		_logger.fine("initialize() called");
 		m_docsProcessed = 0;
 		_stemmer = null;
 	}
 
-	public void dispose(ComponentContextProperties ccp) {
+	public void disposeCallBack(ComponentContextProperties ccp)
+    throws Exception {
 		_logger.fine("dispose() called");
 
 		if (getVerbose(ccp)) {
@@ -190,8 +193,8 @@ public class Stem implements ExecutableComponent {
 		_stemmer = null;
 	}
 
-	public void execute(ComponentContext ctx)
-			throws ComponentExecutionException, ComponentContextException {
+	public void executeCallBack(ComponentContext ctx)
+    throws Exception {
 		_logger.fine("execute() called");
 
 		// props ==============================
@@ -251,6 +254,7 @@ public class Stem implements ExecutableComponent {
 			if (verbose && (m_docsProcessed % 20 == 0)) {
 				System.out.println("Stem: processed " + m_docsProcessed);
 			}
+			componentConsoleHandler.whenLogLevelOutput("info","Stem: processed " + m_docsProcessed);
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
